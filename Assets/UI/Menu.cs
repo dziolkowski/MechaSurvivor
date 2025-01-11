772018e73b90495dd3f5027efbc6b8e0f9322de6
Frost <l.witkowski96@gmail.com>
2025-01-11 23:11:24 +0100

    Added TMP, implemented Main Menu, placeholders for in game menu
    Added TextMeshPro,
    Main menu - missing settings and extras,
    Placeholder setup for in game interface and menu

---
+++ Assets/UI/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    // Called when we click the "Play" button. 
+    public void LoadLevel(int level) {
+        SceneManager.LoadScene(level);
+    }
+    
+    public void OnSettingsButton() {
+        Debug.Log("Open settings menu TBD");
+    }
+
+    public void OnExtrasButton() {
+        Debug.Log("Open extras menu TBD");
+    }
+    // Called when we click the "Quit" button.
+    public void OnQuitButton() {
+        Application.Quit();
+    }
+}
