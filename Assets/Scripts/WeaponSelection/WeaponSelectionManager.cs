d5304f7f1a4b37329976e60a19704e5a015c7510
Dawid Ziółkowski <dziolkowski@edu.cdv.pl>
2025-05-08 12:14:46 +0200

    Added Weapon Selection

---
+++ Assets/Scripts/WeaponSelection/WeaponSelectionManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelectionManager : MonoBehaviour
+{
+    public List<WeaponData> availableWeapons; // Lista wszystkich broni
+    public Transform topSlot; // Referencja do Top Slot
+    public List<Button> topWeaponButtons; // Recznie przypisane przyciski w UI
+    public WeaponDropManager dropManager; // Odniesienie do systemu dropu
+    public Image topSlotIconHUD; // Ikona zmieniana na interfejsie
+
+    void Start()
+    {
+        Time.timeScale = 0; // Pauza
+
+        if (topWeaponButtons.Count != availableWeapons.Count)
+        {
+            Debug.LogWarning("Liczba przycisków nie odpowiada liczbie dostêpnych broni!");
+        }
+
+        for (int i = 0; i < availableWeapons.Count; i++)
+        {
+            var weapon = availableWeapons[i];
+            var button = topWeaponButtons[i];
+
+            // Ustaw ikone na przycisku 
+            Image iconImage = button.transform.Find("Icon").GetComponent<Image>();
+            if (iconImage != null)
+            {
+                iconImage.sprite = weapon.icon;
+            }
+
+            // Potrzebujemy tymczasowej zmiennej, zeby uniknac problemu z referencja w petli
+            WeaponData capturedWeapon = weapon;
+
+            button.onClick.RemoveAllListeners(); // Czyscimy stare eventy
+            button.onClick.AddListener(() => SelectTopWeapon(capturedWeapon));
+        }
+    }
+
+    void SelectTopWeapon(WeaponData weapon)
+    {
+        topSlot.GetComponent<WeaponSlot>().AssignWeapon(weapon);
+
+        // Aktualizowanie ikony na interfejsie
+        if (topSlotIconHUD != null)
+        {
+            topSlotIconHUD.sprite = weapon.icon;
+            topSlotIconHUD.enabled = true; // Upewniamy sie, ze jest widoczna
+        }
+
+        dropManager.Initialize(availableWeapons); // Przekazujemy pozostale bronie do drop systemu
+        gameObject.SetActive(false); // Chowamy UI wyboru
+
+        Time.timeScale = 1; // Wznowienie gry
+    }
+}
--- Assets/Scripts/WeaponSelection/WeaponSlot.cs
+++ Assets/Scripts/WeaponSelection/WeaponSlot.cs
@@ -2,31 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SlotType { Top, Side } // Typ slotu: top lub side
 public class WeaponSlot : MonoBehaviour
 {
-    public SlotType slotType; // Typ slotu
-    private GameObject currentWeaponModel; // Model broni przypiety do slotu
-    private MonoBehaviour currentWeaponScript;// Skrypt broni przypiety do slotu
+    public enum SlotType { Top, Side}
+    public SlotType slotType;
 
-    public void EquipWeapon(WeaponData weaponData) // Funkcja do zalozenia nowej broni
+
+    public void AssignWeapon(WeaponData weapon) 
     {
-        if (currentWeaponModel != null) // Usuwanie broni jesli jest w slocie
-            Destroy(currentWeaponModel);
-
-        if (currentWeaponScript != null)
-            Destroy(currentWeaponScript);
-
-        // Przypisanie odpowiedniego modelu i skryptu do typu slotu
-        if (slotType == SlotType.Top)
-        {
-            currentWeaponModel = Instantiate(weaponData.topModelPrefab, transform);
-            currentWeaponScript = gameObject.AddComponent(System.Type.GetType(weaponData.topWeaponScript.name)) as MonoBehaviour;
-        }
-        else
-        {
-            currentWeaponModel = Instantiate(weaponData.sideModelPrefab, transform);
-            currentWeaponScript = gameObject.AddComponent(System.Type.GetType(weaponData.sideWeaponScript.name)) as MonoBehaviour;
-        }
+        GameObject prefab = slotType == SlotType.Top ? weapon.topSlotPrefab : weapon.sideSlotPrefab;
+        Instantiate(prefab, transform.position, transform.rotation, transform);
     }
 }
