07e9af5eb8fc1d616fd1df26cd1aff3f4b8fd877
LW-Frost <l.witkowski96@gmail.com>
2025-05-16 16:33:47 +0200

    Enemy Event Manager
    Responsible for toggling enemy spawns and changing enemy HP

---
+++ Assets/Scripts/EventManagers/EnemyEventManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBasedEventManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class ScheduledEvent
+    {
+        public enum EventType { IncreaseEnemyHP, ToggleCanSpawn } // Action type
+        public EventType eventType;                              // The type of event
+        public string targetName;                                // Target enemy name (for both events)
+        public int newHealthValue;                               // New health value (only for IncreaseEnemyHP)
+        public bool newCanSpawnValue = true;                            // New canSpawn value (only for ToggleCanSpawn)
+        public float triggerTime;                                // When to trigger (in seconds since game started)
+    }
+
+    [SerializeField] private List<ScheduledEvent> scheduledEvents = new List<ScheduledEvent>(); // Event list
+    [SerializeField] private EnemyManager enemyManager;           // Reference to EnemyManager
+    [SerializeField] private SpawnEnemiesOnNavMesh spawner;       // Reference to OutsideCameraEnemySpawner
+
+    private float gameStartTime;
+
+    void Start()
+    {
+        gameStartTime = Time.time; // Track game start time
+        StartCoroutine(ProcessEvents());
+    }
+
+    private IEnumerator ProcessEvents()
+    {
+        // Iterate through the scheduled events
+        foreach (var scheduledEvent in scheduledEvents)
+        {
+            float waitTime = scheduledEvent.triggerTime - (Time.time - gameStartTime); // Time left
+            if (waitTime > 0)
+            {
+                yield return new WaitForSeconds(waitTime); // Wait until trigger time
+            }
+
+            ExecuteEvent(scheduledEvent); // Trigger the event
+        }
+    }
+
+    private void ExecuteEvent(ScheduledEvent scheduledEvent)
+    {
+        switch (scheduledEvent.eventType)
+        {
+            case ScheduledEvent.EventType.IncreaseEnemyHP:
+                if (enemyManager != null)
+                {
+                    enemyManager.SetEnemyHealth(scheduledEvent.targetName, scheduledEvent.newHealthValue);
+                }
+                else
+                {
+                    Debug.LogError("EnemyManager reference is missing!");
+                }
+                break;
+
+            case ScheduledEvent.EventType.ToggleCanSpawn:
+                if (spawner != null)
+                {
+                    if (scheduledEvent.newCanSpawnValue)
+                    {
+                        spawner.StartSpawning(scheduledEvent.targetName); // Start spawning if newCanSpawnValue is true
+                    }
+                    else
+                    {
+                        spawner.StopSpawning(scheduledEvent.targetName); // Stop spawning if newCanSpawnValue is false
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Spawner reference is missing!");
+                }
+                break;
+
+            default:
+                Debug.LogWarning("Unknown event type!");
+                break;
+        }
+    }
+}
