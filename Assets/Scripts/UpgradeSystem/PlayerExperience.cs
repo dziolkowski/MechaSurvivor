35d24085f6ae90fa1ec7e78d7d2a8b9ebafaf6b1
Frost <l.witkowski96@gmail.com>
2025-05-19 21:14:47 +0200

    Exp to next lvl multiplayer exposed to inspector

--- Assets/Scripts/UpgradeSystem/PlayerExperience.cs
+++ Assets/Scripts/UpgradeSystem/PlayerExperience.cs
@@ -6,6 +6,7 @@ public class PlayerExperience : MonoBehaviour
     public int level = 0;
     public int currentExp = 0;
     public int expToNextLevel = 100;
+    [SerializeField] private float expMultiplier = 1.25f;
 
     public UpgradeUIManager upgradeUIManager; // Przypisz w Inspectorze
     [SerializeField] private List<StatUpgradeData> allUpgrades; // Dodaj tutaj upgrady
@@ -26,7 +27,7 @@ public class PlayerExperience : MonoBehaviour
         level++;
         currentExp -= expToNextLevel;
 
-        expToNextLevel = Mathf.RoundToInt(expToNextLevel * 1.25f); // Koszt kolejnego levelu
+        expToNextLevel = Mathf.RoundToInt(expToNextLevel * expMultiplier); // Koszt kolejnego levelu
 
         // Pokazujemy ekran wyboru upgradow
         upgradeUIManager.ShowUpgradeChoices();
